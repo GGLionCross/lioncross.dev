b7cb145f9dacf9983d9e3bfeeb9a0d50800bbfc4
GGLionCross <lionelsquiambao@gmail.com>
2025-08-26 03:25:04 -0700

    Commented out the About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import HeroSection from "./_sections/HeroSection";
-import AboutSection from "./_sections/AboutSection";
+// import AboutSection from "./_sections/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <AboutSection />
+      {/* <AboutSection /> */}
     </main>
   );
 }
