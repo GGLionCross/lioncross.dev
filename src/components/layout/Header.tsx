903d353c1f8a1880f6dbfa148c9c803775431ff4
GGLionCross <lionelsquiambao@gmail.com>
2025-08-28 13:37:31 -0700

    Commented out the menu icon in the header until we're ready to add more pages

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -51,13 +51,13 @@ export default function Header() {
           </nav>
 
           {/* Mobile menu button */}
-          <div className="md:hidden">
+          {/* <div className="md:hidden">
             <button className="text-gray-300 hover:text-pink-300 transition-colors duration-300">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </header>
